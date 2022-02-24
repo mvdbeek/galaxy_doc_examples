9079a1e560669e2f19236d66f2c428fcaac6c2e8
mvdbeek <m.vandenbeek@gmail.com>
2022-02-24 13:04:27 +0100

    Cleanup

--- apache/osx_wrapper.sh
+++
@@ -1,5 +0,0 @@
-# socat TCP-LISTEN:4444,reuseaddr,fork "UNIX-CLIENT:/tmp/gunicorn.sock" &
-# socat_pid=$!
-# trap "kill -- $socat_pid" EXIT
-
-docker run --rm --name apache -p 8888:80 -p 8843:443 -p 4444 -it -v ~/src/galaxy:/srv/galaxy/server -v $(pwd)/server.crt:/etc/apache2/ssl/server.crt -v $(pwd)/server.key:/etc/apache2/ssl/server.key  httpd
--- apache/run_apache_proxy.sh
+++ apache/run_apache_proxy.sh
@@ -1,2 +1,6 @@
 docker build -t httpd .
+socat TCP-LISTEN:4444,reuseaddr,fork "UNIX-CLIENT:/tmp/gunicorn.sock" &
+socat_pid=$!
+trap "kill -- $socat_pid" EXIT
+
 docker run --rm --name apache -h localhost -p 8888:80 -p 8843:443 -p 4444 -it -v ~/src/galaxy:/srv/galaxy/server -v $(pwd)/server.crt:/etc/apache2/ssl/server.crt -v $(pwd)/server.key:/etc/apache2/ssl/server.key  httpd
