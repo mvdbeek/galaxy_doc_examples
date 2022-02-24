620e7f74916c9959566ced3da5ce8d2633c5bf2f
mvdbeek <m.vandenbeek@gmail.com>
2022-02-24 17:02:28 +0100

    Fix url scheme

--- apache/run_apache_proxy.sh
+++ apache/run_apache_proxy.sh
@@ -11,4 +11,4 @@ case $OS in
   *) ;;
 esac
 
-docker run --rm --name apache -h localhost -p 8888:80 -p 8843:443 -p 4444 -it -v ~/src/galaxy:/srv/galaxy/server -v $(pwd)/server.crt:/etc/apache2/ssl/server.crt -v $(pwd)/server.key:/etc/apache2/ssl/server.key  httpd
+docker run --rm --name apache -h localhost -p 8888:80 -p 443:443 -p 4444 -it -v ~/src/galaxy:/srv/galaxy/server -v $(pwd)/server.crt:/etc/apache2/ssl/server.crt -v $(pwd)/server.key:/etc/apache2/ssl/server.key  httpd
