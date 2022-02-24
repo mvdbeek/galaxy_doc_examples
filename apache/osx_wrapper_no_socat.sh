ca895ee9b33afb54509c61fa87d1bb6815fe0cd0
mvdbeek <m.vandenbeek@gmail.com>
2022-02-24 13:01:46 +0100

    Works with socket

--- apache/osx_wrapper_no_socat.sh
+++ apache/osx_wrapper_no_socat.sh
@@ -1,1 +1,2 @@
+docker build -t httpd .
 docker run --rm --name apache -h localhost -p 8888:80 -p 8843:443 -p 4444 -it -v ~/src/galaxy:/srv/galaxy/server -v $(pwd)/server.crt:/etc/apache2/ssl/server.crt -v $(pwd)/server.key:/etc/apache2/ssl/server.key  httpd
