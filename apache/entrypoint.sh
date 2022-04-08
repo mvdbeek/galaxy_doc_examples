3661eb105ab1d314a99c355d7e94efa52037c1df
mvdbeek <m.vandenbeek@gmail.com>
2022-04-08 11:42:34 +0200

    Working tusd config for apache
    Not good though, shouldn't have to change the base path, that'd be a
    little unfortunate with gravity.

--- apache/entrypoint.sh
+++ apache/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-set -e
+set -xe
 
 umask 0
 socat UNIX-LISTEN:/srv/galaxy/var/gunicorn.sock,fork TCP:host.docker.internal:4444 &
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
+
+# Start tusd server
+tusd -host localhost -behind-proxy -port 1080 -upload-dir=/tmp -hooks-http=http://host.docker.internal:4444/api/upload/hooks -hooks-http-forward-headers=X-Api-Key,Cookie -base-path=/api/upload/resumable_upload &
 
 exec httpd -DFOREGROUND "$@"
